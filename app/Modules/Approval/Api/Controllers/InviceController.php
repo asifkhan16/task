e8dd1a5548ca90f7e439800eb4aea1a41ec86512
asif khan <asiff080@gmail.com>
2023-04-25 01:43:35 +0500

    create InviceController

---
+++ app/Modules/Approval/Api/Controllers/InviceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Approval\Api\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InviceController extends Controller
+{
+    public function index(){
+        
+    }
+}
