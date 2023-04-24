72c0dc064312e5022a17e33e2d41d6d47b237aab
asif khan <asiff080@gmail.com>
2023-04-25 01:57:53 +0500

    create InvoiceProductLine model

---
+++ app/Modules/Invoices/Infrastructure/Models/InvoiceProductLine.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Invoices\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceProductLine extends Model
+{
+    use HasFactory;
+}
